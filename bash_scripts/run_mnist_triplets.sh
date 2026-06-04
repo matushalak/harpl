7d7e6e10e935d97d93435b690c067bbe5ac34ae6
matushalak <matus.halak@gmail.com>
2026-06-04 12:55:23 +0200

    Set CPU device in run wrappers

--- bash_scripts/run_mnist_triplets.sh
+++ bash_scripts/run_mnist_triplets.sh
@@ -1,7 +1,9 @@
+DEVICE="${HARPL_DEVICE:-cpu}"
+
 for i in {1..3}; do
-    bash bash_scripts/mnist_triplets.sh --seed $i --experiment_name mnist_triplets_pred_$i --loss pred --prediction_target enc;
-    bash bash_scripts/mnist_triplets.sh --seed $i --experiment_name mnist_triplets_inv_sg_$i --loss inv --prediction_target pred --pull_coef 1.0 --push_coef 1.0 --decorr_coef 10.0;
+    bash bash_scripts/mnist_triplets.sh --device "$DEVICE" --seed $i --experiment_name mnist_triplets_pred_$i --loss pred --prediction_target enc;
+    bash bash_scripts/mnist_triplets.sh --device "$DEVICE" --seed $i --experiment_name mnist_triplets_inv_sg_$i --loss inv --prediction_target pred --pull_coef 1.0 --push_coef 1.0 --decorr_coef 10.0;
 done
 
-bash bash_scripts/mnist_triplets.sh --seed $i --experiment_name mnist_triplets_supervised_$i --loss supervised;
-bash bash_scripts/mnist_triplets.sh --seed $i --experiment_name mnist_triplets_random_$i --loss supervised --freeze;
+bash bash_scripts/mnist_triplets.sh --device "$DEVICE" --seed $i --experiment_name mnist_triplets_supervised_$i --loss supervised;
+bash bash_scripts/mnist_triplets.sh --device "$DEVICE" --seed $i --experiment_name mnist_triplets_random_$i --loss supervised --freeze;

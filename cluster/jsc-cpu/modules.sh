fc7ec4d7c6bf5861268dc468a882685f88fea07c
matushalak <matus.halak@gmail.com>
2026-06-04 13:46:09 +0200

    .

--- cluster/jsc-cpu/modules.sh
+++ cluster/jsc-cpu/modules.sh
@@ -3,6 +3,6 @@ module load Stages/2026
 module load GCC
 
 # Base scientific Python modules recommended by the JSC sc_venv_template.
-module load mpi4py numba tqdm matplotlib IPython SciPy-Stack bokeh git
+module load numba tqdm matplotlib IPython SciPy-Stack bokeh git
 module load Flask Seaborn
 module load scikit-learn tensorboard h5py
